832212739092420725de586b5a1914a5f1d8beb6
熊 春雷 <pandaxcl@gmail.com>
2016-04-18 11:03:53 +0800

    1. 添加了storyboard里面的outlet 2. 用响应式编程的方式把界面逻辑做掉了

--- study-reactive-login/ViewController.m
+++ study-reactive-login/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *textField_account;
+@property (weak, nonatomic) IBOutlet UITextField *textField_password;
+@property (weak, nonatomic) IBOutlet UIButton *button_login;
 @end
 
 @implementation ViewController
@@ -17,6 +20,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    RACSignal*signalAccount = [self.textField_account.rac_textSignal map:^id(NSString* value) {
+        return @([value length] >= 3);
+    }];
+    RACSignal*signalPassword = [self.textField_password.rac_textSignal map:^id(NSString* value) {
+        return @([value length] >= 6);
+    }];
+    RACSignal*signalLoginEnabled = [[signalAccount combineLatestWith:signalPassword] map:^id(RACTuple* value) {
+        RACTupleUnpack(NSNumber* accountIsValid, NSNumber* passwordIsValid) = value;
+        return @([accountIsValid boolValue] && [passwordIsValid boolValue]);
+    }];
+    typeof(self) __weak wself = self;
+    [signalLoginEnabled subscribeNext:^(id x) {
+        typeof(self) sself = wself;
+        sself.button_login.enabled = [x boolValue];
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
