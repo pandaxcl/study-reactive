f2418acdb7aaa0770f1fce2d3e6d47514cdd0782
熊 春雷 <pandaxcl@gmail.com>
2016-04-27 10:38:51 +0800

    1. 打开了非https得http请求的许可 2. AFNetworking的第一个请求终于成功了；）

--- study-reactive-login/ViewController.m
+++ study-reactive-login/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <AFNetworking/AFNetworking.h>
+
 @interface ViewController ()
 
 @end
@@ -17,6 +20,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    AFHTTPSessionManager*manager = [AFHTTPSessionManager manager];
+    
+    manager.responseSerializer = [AFXMLParserResponseSerializer new];
+    
+    NSSet*contentTypes = [NSSet setWithObjects:@"text/html", nil];
+    manager.responseSerializer.acceptableContentTypes = contentTypes;
+    
+    [manager GET:@"http://www.csdn.net" parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"responseObject = %@", responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"         error = %@", error);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
